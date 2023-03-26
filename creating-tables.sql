3e5d374125b9e3c4a56ff511f4547bb5390da95e
Daniel Chaves <daniel.cassiano@live.com>
2023-03-26 17:04:50 -0300

    Create creating-tables.sql

---
+++ creating-tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `pessoas` (
+id int NOT NULL AUTO_INCREMENT, 
+nome VARCHAR(30) NOT NULL,
+nascimento DATE,
+sexo ENUM('M','F'),
+peso DECIMAL(5,2),
+altura DECIMAL(3,2),
+nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8;
