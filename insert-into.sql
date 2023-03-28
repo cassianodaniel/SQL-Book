4e746003489cf0bbea1b46393e804662506d3efd
Daniel Chaves <daniel.cassiano@live.com>
2023-03-28 16:33:04 -0300

    Create insert-into.sql

---
+++ insert-into.sql
@@ -0,0 +1,6 @@
+INSERT INTO pessoas
+(id, nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES 
+(DEFAULT, 'Daniel', '2000-04-26', 'M', '95.2', '1.75', 'Brasil');
+
+INSERT INTO pessoas VALUES (DEFAULT, 'Daniel', '2000-04-26', 'M', '95.2', '1.75', 'Brasil');
